0c63b57f318c02a463484ba2802c9a5ae65f289c
MrWillian <willian.dev10@gmail.com>
2022-12-15 15:06:13 -0300

    refactor(mapper): add toDomain in PrismaNotificationMapper

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from "@prisma/client";
 import { Notification } from "@application/entities/notification/notification";
+import { Content } from "@application/entities/notification/content";
 
 export class PrismaNotificationMapper {
     static toPrisma(notification: Notification) {
@@ -8,7 +10,22 @@ export class PrismaNotificationMapper {
             content: notification.content.value,
             recipientId: notification.recipientId,
             readAt: notification.readAt,
-            createdAt: notification.createdAt
+            createdAt: notification.createdAt,
+            canceledAt: notification.canceledAt
         }
+    }
+
+    static toDomain(raw: RawNotification): Notification {
+        return new Notification(
+            {
+                categories: raw.categories,
+                content: new Content(raw.content),
+                recipientId: raw.recipientId,
+                readAt: raw.readAt,
+                canceledAt: raw.canceledAt,
+                createdAt: raw.createdAt
+            },
+            raw.id
+        );
     }
 }
